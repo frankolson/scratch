7e085c86bf36f2df3a5560ebf1c10175b5035d55
Will Olson <will.olson11@gmail.com>
2017-09-02 11:22:40 -0700

    chore: House keeping

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// Vendor Assets
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Project Assets
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
